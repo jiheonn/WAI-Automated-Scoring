eb753050f635857037109efaf66bda267cf2b2d2
Jiheon Lee <wlgjsdl0@gmail.com>
2020-03-16 20:02:25 +0900

    add teacher base files

--- wa3i/teacher/urls.py
+++ wa3i/teacher/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='teacher'),
+    path('question_selection/', views.question_selection, name='question_selection'),
+    path('view_result/', views.view_result, name='view_result'),
+    path('make_question/', views.make_question, name='make_question'),
+    path('bigram_tree/', views.bigram_tree, name='bigram_tree'),
+    path('topic_analysis/', views.topic_analysis, name='topic_analysis'),
+    path('response_analysis/', views.response_analysis, name='response_analysis'),
+    path('qr_code/', views.qr_code, name='qr_code'),
+    path('notice/', views.notice, name='notice'),
 ]
