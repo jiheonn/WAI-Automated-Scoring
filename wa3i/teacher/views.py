adb84e2e04573927cd6a6dd7c82c59e9760f3957
Jiheon Lee <wlgjsdl0@gmail.com>
2020-05-07 21:46:35 +0900

    Update files

--- wa3i/teacher/views.py
+++ wa3i/teacher/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from mainpage.models import Question, Assignment, AssignmentQuestionRel, Solve, Keyword, Category
+from django.http import JsonResponse
+from django.db.models import Q
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 # def index(request):
@@ -9,18 +12,23 @@ from django.http import HttpResponse
 
 def index(request):
     context = {
+
     }
     return render(request, 'teacher/index.html', context)
 
 
 def question_selection(request):
+    question_data = Question.objects.all()
     context = {
+        'question_data': question_data
     }
     return render(request, 'teacher/question_selection.html', context)
 
 
 def view_result(request):
+    assignment_data = Assignment.objects.all()
     context = {
+        'assignment_data': assignment_data
     }
     return render(request, 'teacher/view_result.html', context)
 
@@ -50,7 +58,9 @@ def response_analysis(request):
 
 
 def qr_code(request):
+    question_data = Question.objects.all()
     context = {
+        'question_data': question_data
     }
     return render(request, 'teacher/QR_code.html', context)
 
@@ -71,3 +81,136 @@ def ex_response_analysis(request):
     context = {
     }
     return render(request, 'teacher/ex_response_analysis.html', context)
+
+
+def view_result_detail(request):
+    select_code = request.GET['select_code']
+    asi_data = AssignmentQuestionRel.objects.select_related('solve', 'assignment').filter(assignment_id=select_code)
+    question_count = asi_data.count()
+
+    assignment_data = []
+    for i in asi_data:
+        assignment_data_dict = dict()
+        assignment_data_dict['assignment_id'] = i.assignment_id
+        assignment_data_dict['assignment_title'] = i.assignment.assignment_title
+        assignment_data_dict['assignment_type'] = i.assignment.type
+        assignment_data_dict['start_date'] = i.assignment.start_date
+        assignment_data_dict['end_date'] = i.assignment.end_date
+        assignment_data_dict['student_grade'] = i.assignment.grade
+        assignment_data_dict['student_class'] = i.assignment.class_field
+        assignment_data_dict['question_count'] = question_count
+        assignment_data.append(assignment_data_dict)
+
+    solve_data = []
+    for j in asi_data:
+        solve_data_dict = dict()
+        solve_data_dict['student_id'] = j.solve.student_id
+        solve_data_dict['student_name'] = j.solve.student_name
+        solve_data_dict['student_score'] = j.solve.score
+        solve_data_dict['question_id'] = j.question_id
+        solve_data_dict['student_response'] = j.solve.response
+        solve_data.append(solve_data_dict)
+
+    context = {
+        'assignment_data': assignment_data,
+        'solve_data': solve_data
+    }
+    return JsonResponse(context)
+
+
+def change_qr_code(request):
+    question_name = request.GET['question_name']
+    qst_data = Question.objects.all().filter(question_name=question_name)
+
+    question_data = []
+    for i in qst_data:
+        question_data_dict = dict()
+        question_data_dict['QR_code'] = i.qr_code
+        question_data.append(question_data_dict)
+
+    context = {
+        'question_data': question_data
+    }
+    return JsonResponse(context)
+
+
+def question_search(request):
+    user_input = request.GET['user_input']
+    sah_data = Keyword.objects.select_related('question').filter(keyword_name__icontains=user_input)
+
+    search_data = []
+    for i in sah_data:
+        search_data_dict = dict()
+        search_data_dict['question_id'] = i.question.question_id
+        search_data_dict['question_name'] = i.question.question_name
+        search_data_dict['question_image'] = i.question.image
+        search_data.append(search_data_dict)
+
+    context = {
+        'search_data': search_data
+    }
+    return JsonResponse(context)
+
+
+def view_search(request):
+    user_input = request.GET['user_input']
+    asi_data = Assignment.objects.all().filter(assignment_title__icontains=user_input)
+
+    assignment_data = []
+    for i in asi_data:
+        assignment_data_dict = dict()
+        assignment_data_dict['assignment_id'] = i.assignment_id
+        assignment_data_dict['assignment_title'] = i.assignment_title
+        assignment_data_dict['assignment_type'] = i.type
+        assignment_data_dict['start_date'] = i.start_date
+        assignment_data_dict['end_date'] = i.end_date
+        assignment_data_dict['student_grade'] = i.grade
+        assignment_data_dict['student_class'] = i.class_field
+        assignment_data.append(assignment_data_dict)
+
+    print(assignment_data)
+
+    context = {
+        'assignment_data': assignment_data
+    }
+    return JsonResponse(context)
+
+
+def assignment_copy(request):
+    copy_code = request.GET['copy_code']
+    print(copy_code)
+    cp_data = AssignmentQuestionRel.objects.select_related('assignment', 'question').filter(assignment_id=copy_code)
+
+    copy_data = []
+    for i in cp_data:
+        copy_data_dict = dict()
+        copy_data_dict['question_id'] = i.question.question_id
+        copy_data_dict['assignment_type'] = i.assignment.type
+        copy_data_dict['assignment_title'] = i.assignment.assignment_title
+        copy_data.append(copy_data_dict)
+    print(copy_data)
+    context = {
+        'copy_data': copy_data
+    }
+    return JsonResponse(context)
+
+
+def change_category(request):
+    category_option = request.GET['option']
+    print(category_option)
+    opt_data = Question.objects.select_related('category').filter(category__category_name=category_option)
+
+    option_data = []
+    for i in opt_data:
+        option_data_dict = dict()
+        option_data_dict['question_id'] = i.question_id
+        option_data_dict['question_name'] = i.question_name
+        option_data_dict['question_image'] = i.image
+        option_data.append(option_data_dict)
+
+    print(option_data)
+
+    context = {
+        'option_data': option_data
+    }
+    return JsonResponse(context)
