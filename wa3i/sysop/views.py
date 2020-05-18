033233555f3c9c16bcd7059f998b84dbaf14c257
JONGSKY <ljhljh0125@gmail.com>
2020-05-18 23:09:32 +0900

    reload sysop files

--- wa3i/sysop/views.py
+++ wa3i/sysop/views.py
@@ -14,10 +14,14 @@ def index(request):
     return render(request, 'sysop/index.html', context)
 
 def teacher_data(request):
-    if request.GET.get('evaluation_type'):
-        featured_filter = request.GET.get('evaluation_type')
-        teacher = Teacher.objects.filter()
+    teacher_id = request.GET.get('teacher_id')
+    # print(teacher)
+    if teacher_id == None:
+        teacher = Teacher.objects.all()
     else:
+        teacher_info = Teacher.objects.get(teacher_id=teacher_id)
+        teacher_info.approve = 1
+        teacher_info.save()
         teacher = Teacher.objects.all()
 
     context = {'teacher':teacher}
@@ -37,8 +41,8 @@ def quiz_produce(request):
 
 def quiz_download(request):
     makequestion = MakeQuestion.objects.all()
-    question = Question.objects.select_related('category')
-    context = {'makequestion':makequestion, 'question':question
+    category = Category.objects.all()
+    context = {'makequestion':makequestion, 'category':category
     }
     return render(request, 'sysop/quiz_download.html', context)
 
@@ -48,7 +52,8 @@ def notice(request):
     return render(request, 'sysop/notice.html', context)
 
 def detailed_review(request):
-    makequestion = MakeQuestion.objects.all()
+    question_id = request.GET['question_id']
+    makequestion = MakeQuestion.objects.select_related('teacher').filter(make_question_id=question_id)[0]
     context = {'makequestion':makequestion
     }
     return render(request, 'sysop/detailed_review.html', context)
