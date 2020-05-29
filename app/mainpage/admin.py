9b680bfb9755020e696271dad9c2eae284951393
JONGSKY <ljhljh0125@gmail.com>
2020-05-29 16:40:02 +0900

    modify admin and models file to replace database with sqlite3

--- app/mainpage/admin.py
+++ app/mainpage/admin.py
@@ -1,3 +1,128 @@
+# admin.py
 from django.contrib import admin
+from .models import *
+from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 
-# Register your models here.
+
+class MarkAdmin(admin.ModelAdmin):
+    # list_display = [field.name for field in Mark._meta.get_fields()]
+    list_display = ['mark_id', 'get_make_question', 'mark_text']
+    def get_make_question(self, obj):
+        return obj.make_question.question_name
+    ordering = ['mark_id']
+    get_make_question.short_description = 'question Name'
+    search_fields = ['make_question__question_name']
+admin.site.register(Mark, MarkAdmin)
+
+class SelfSolveDataAdmin(admin.ModelAdmin):
+    list_display = ['self_id', 'get_make_question', 'response', 'score', 'submit_date']
+    def get_make_question(self, obj):
+        return obj.make_question.question_name
+    ordering = ['self_id']
+    get_make_question.short_description = 'question Name'
+    list_filter = ('score',)
+    search_fields = ['make_question__question_name']
+admin.site.register(SelfSolveData, SelfSolveDataAdmin)
+
+class MakeQuestionAdmin(admin.ModelAdmin):
+    list_display = ['make_question_id', 'get_teacher_name', 'question_name','made_date', 'upload_check']
+    def get_teacher_name(self, obj):
+        return obj.teacher.teacher_name
+    ordering = ['make_question_id']
+    get_teacher_name.short_description = 'teacher name'
+
+    list_filter = ('upload_check',)
+    search_fields = ['teacher__teacher_name', 'question_name']
+admin.site.register(MakeQuestion, MakeQuestionAdmin)
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['teacher_id', 'teacher_name', 'school', 'email',
+                    'password', 'approve']
+    ordering = ['teacher_id']
+    list_filter = ('approve',)
+    search_fields = ['teacher_name', 'school']
+admin.site.register(Teacher, TeacherAdmin)
+
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ['assignment_id', 'get_teacher_name', 'assignment_title', 'type',
+                    'start_date', 'end_date', 'made_date', 'grade', 'school_class']
+    def get_teacher_name(self, obj):
+        return obj.teacher.teacher_name
+    ordering = ['made_date']
+    get_teacher_name.short_description = 'teacher name'
+    list_filter = ('type',
+                   ('made_date', DateRangeFilter)
+                   )
+    search_fields = ['assignment_title', 'teacher']
+admin.site.register(Assignment, AssignmentAdmin)
+
+class StudySolveDataAdmin(admin.ModelAdmin):
+    list_display = ['study_id', 'get_question_name', 'school', 'gender',
+                    'response', 'score', 'submit_date']
+    def get_question_name(self, obj):
+        return obj.question.question_name
+    ordering = ['study_id']
+    get_question_name.short_description = 'question_name'
+    list_filter = ('score', 'school', 'gender',
+                   ('submit_date', DateRangeFilter),
+                   )
+admin.site.register(StudySolveData, StudySolveDataAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_id', 'category_name']
+    ordering = ['category_id']
+admin.site.register(Category, CategoryAdmin)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_id','get_category_name', 'model_id', 'question_name',
+                    'made_date', 'ques_concept']
+    def get_category_name(self, obj):
+        return obj.category.category_name
+    ordering = ['question_id']
+    get_category_name.short_description = 'category_name'
+    list_filter = ('category__category_name',
+                   ('made_date', DateRangeFilter),
+                   )
+admin.site.register(Question, QuestionAdmin)
+
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ['keyword_id', 'get_question_name','keyword_name']
+    def get_question_name(self, obj):
+        return obj.question.question_name
+    ordering = ['keyword_id']
+    get_question_name.short_description = 'question_name'
+admin.site.register(Keyword, KeywordAdmin)
+
+
+class SolveAdmin(admin.ModelAdmin):
+    list_display = ['solve_id', 'student_name', 'get_assignment_name', 'get_question_name',
+                    'student_id', 'submit_date', 'response','score']
+    def get_assignment_name(self, obj):
+        return obj.as_qurel.assignment.assignment_title
+    def get_question_name(self, obj):
+        return obj.as_qurel.question.question_name
+    ordering = ['solve_id']
+    get_assignment_name.short_description = 'assignment_name'
+    get_question_name.short_description = 'question_name'
+    list_filter = (
+                   ('submit_date', DateRangeFilter),
+                   )
+    search_fields = ['student_name', 'as_qurel__assignment__assignment_title',
+                     'as_qurel__question__question_name', 'student_id']
+admin.site.register(Solve, SolveAdmin)
+
+class AssignmentQuestionRelAdmin(admin.ModelAdmin):
+    list_display = ['as_qurel_id', 'get_assignment_id', 'get_assignment_name', 'get_question_name']
+    def get_assignment_id(self, obj):
+        return obj.assignment.assignment_id
+    def get_assignment_name(self, obj):
+        return obj.assignment.assignment_title
+    def get_question_name(self, obj):
+        return obj.question.question_name
+    ordering = ['as_qurel_id']
+    get_assignment_id.short_description = 'assignment_id'
+    get_assignment_name.short_description = 'assignment_name'
+    get_question_name.short_description = 'question_name'
+    search_fields = ['assignment__assignment_id', 'assignment__assignment_title',
+                     'question__question_name']
+admin.site.register(AssignmentQuestionRel, AssignmentQuestionRelAdmin)
--- app/mainpage/models.py
+++ app/mainpage/models.py
@@ -1,151 +1,89 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
+# Create your models here.
+class Teacher(models.Model):
+    teacher_id = models.AutoField(primary_key=True, auto_created=True)
+    teacher_name = models.CharField(max_length=50)
+    school = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    approve = models.IntegerField()
 
 class Assignment(models.Model):
     assignment_id = models.CharField(primary_key=True, max_length=50)
-    teacher = models.ForeignKey('Teacher', models.DO_NOTHING, blank=True, null=True)
-    assignment_title = models.CharField(max_length=50, blank=True, null=True)
-    type = models.CharField(max_length=50, blank=True, null=True)
-    start_date = models.DateField(blank=True, null=True)
-    end_date = models.DateField(blank=True, null=True)
-    made_date = models.DateField(blank=True, null=True)
-    grade = models.IntegerField(blank=True, null=True)
-    class_field = models.IntegerField(db_column='class', blank=True, null=True)  # Field renamed because it was a Python reserved word.
-
-    class Meta:
-        managed = False
-        db_table = 'assignment'
-
-
-class AssignmentQuestionRel(models.Model):
-    as_qurel_id = models.IntegerField(primary_key=True)
-    assignment = models.ForeignKey(Assignment, models.DO_NOTHING, blank=True, null=True)
-    question = models.ForeignKey('Question', models.DO_NOTHING, blank=True, null=True)
-    solve = models.ForeignKey('Solve', models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'assignment_question_rel'
-
-
-class Category(models.Model):
-    category_id = models.IntegerField(primary_key=True)
-    category_name = models.CharField(max_length=50, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'category'
-
-
-class CategoryQuestionRel(models.Model):
-    cate_qurel_id = models.IntegerField(primary_key=True)
-    question = models.ForeignKey('Question', models.DO_NOTHING, blank=True, null=True)
-    category = models.ForeignKey(Category, models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'category_question_rel'
-
-
-class Keyword(models.Model):
-    keyword_id = models.IntegerField(primary_key=True)
-    question = models.ForeignKey('Question', models.DO_NOTHING, blank=True, null=True)
-    keyword_name = models.CharField(max_length=50, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'keyword'
-
+    teacher = models.ForeignKey('Teacher', models.DO_NOTHING)
+    assignment_title = models.CharField(max_length=200)
+    type = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    made_date = models.DateField()
+    grade = models.IntegerField()
+    school_class = models.IntegerField()
 
 class MakeQuestion(models.Model):
-    make_question_id = models.IntegerField(primary_key=True)
-    teacher = models.ForeignKey('Teacher', models.DO_NOTHING, blank=True, null=True)
-    question_name = models.CharField(max_length=50, blank=True, null=True)
-    discription = models.TextField(blank=True, null=True)
-    answer = models.CharField(max_length=255, blank=True, null=True)
-    image = models.TextField(blank=True, null=True)
-    hint = models.CharField(max_length=200, blank=True, null=True)
-    ques_concept = models.CharField(max_length=255, blank=True, null=True)
-    check = models.IntegerField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'make_question'
-
-
-class Question(models.Model):
-    question_id = models.IntegerField(primary_key=True)
-    category = models.ForeignKey(Category, models.DO_NOTHING, blank=True, null=True)
-    model_id = models.CharField(max_length=50, blank=True, null=True)
-    question_name = models.CharField(max_length=50, blank=True, null=True)
-    discription = models.TextField(blank=True, null=True)
-    answer = models.CharField(max_length=255, blank=True, null=True)
-    image = models.CharField(max_length=50, blank=True, null=True)
-    hint = models.CharField(max_length=200, blank=True, null=True)
-    made_date = models.DateField(blank=True, null=True)
-    qr_code = models.CharField(db_column='QR_code', max_length=50, blank=True, null=True)  # Field name made lowercase.
-    ques_concept = models.CharField(max_length=255, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'question'
-
+    make_question_id = models.AutoField(primary_key=True, auto_created=True)
+    teacher = models.ForeignKey('Teacher', models.DO_NOTHING)
+    question_name = models.CharField(max_length=200)
+    discription = models.TextField()
+    answer = models.TextField()
+    image = models.CharField(max_length=200)
+    hint = models.TextField()
+    made_date = models.DateField()
+    upload_check = models.IntegerField()
 
 class SelfSolveData(models.Model):
-    self_id = models.IntegerField(primary_key=True)
-    make_question = models.ForeignKey(MakeQuestion, models.DO_NOTHING, blank=True, null=True)
-    response = models.TextField(blank=True, null=True)
-    score = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
-    submit_date = models.DateField(blank=True, null=True)
+    self_id = models.AutoField(primary_key=True, auto_created=True)
+    make_question = models.ForeignKey(MakeQuestion, models.DO_NOTHING)
+    response = models.TextField()
+    score = models.DecimalField(max_digits=5, decimal_places=2)
+    submit_date = models.DateField()
 
-    class Meta:
-        managed = False
-        db_table = 'self_solve_data'
+class Mark(models.Model):
+    mark_id = models.AutoField(primary_key=True, auto_created=True)
+    make_question = models.ForeignKey(MakeQuestion, models.DO_NOTHING)
+    mark_text = models.TextField()
 
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True, auto_created=True)
+    category_name = models.CharField(max_length=50)
 
-class Solve(models.Model):
-    solve_id = models.IntegerField(primary_key=True)
-    student_id = models.IntegerField(blank=True, null=True)
-    as_qurel_id = models.IntegerField(blank=True, null=True)
-    modified_date = models.DateField(blank=True, null=True)
-    response = models.TextField(blank=True, null=True)
-    score = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
-    student_name = models.CharField(max_length=50, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'solve'
-
+class Question(models.Model):
+    question_id = models.AutoField(primary_key=True, auto_created=True)
+    category = models.ForeignKey(Category, models.DO_NOTHING)
+    model_id = models.CharField(max_length=50)
+    question_name = models.CharField(max_length=100)
+    discription = models.TextField()
+    answer = models.TextField()
+    image = models.CharField(max_length=200)
+    hint = models.TextField()
+    made_date = models.DateField()
+    qr_code = models.CharField(max_length=100)  # Field name made lowercase.
+    ques_concept = models.CharField(max_length=255)
 
 class StudySolveData(models.Model):
-    study_id = models.IntegerField(primary_key=True)
-    question = models.ForeignKey(Question, models.DO_NOTHING, blank=True, null=True)
-    school = models.CharField(max_length=30, blank=True, null=True)
-    sex = models.CharField(max_length=30, blank=True, null=True)
-    response = models.TextField(blank=True, null=True)
-    score = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
-    submit_date = models.DateField(blank=True, null=True)
+    study_id = models.AutoField(primary_key=True, auto_created=True)
+    question = models.ForeignKey(Question, models.DO_NOTHING)
+    school = models.CharField(max_length=30)
+    gender = models.CharField(max_length=30)
+    response = models.TextField()
+    score = models.DecimalField(max_digits=5, decimal_places=2)
+    submit_date = models.DateField()
 
-    class Meta:
-        managed = False
-        db_table = 'study_solve_data'
+class Keyword(models.Model):
+    keyword_id = models.AutoField(primary_key=True, auto_created=True)
+    question = models.ForeignKey('Question', models.DO_NOTHING)
+    keyword_name = models.CharField(max_length=50)
 
+class AssignmentQuestionRel(models.Model):
+    as_qurel_id = models.AutoField(primary_key=True, auto_created=True)
+    assignment = models.ForeignKey(Assignment, models.DO_NOTHING)
+    question = models.ForeignKey('Question', models.DO_NOTHING)
 
-class Teacher(models.Model):
-    teacher_id = models.IntegerField(primary_key=True)
-    teacher_name = models.CharField(max_length=50, blank=True, null=True)
-    school = models.CharField(max_length=50, blank=True, null=True)
-    email = models.CharField(max_length=50, blank=True, null=True)
-    password = models.CharField(max_length=50, blank=True, null=True)
-    approve = models.CharField(max_length=50, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'teacher'
+class Solve(models.Model):
+    solve_id = models.AutoField(primary_key=True, auto_created=True)
+    as_qurel = models.ForeignKey(AssignmentQuestionRel, models.DO_NOTHING)
+    student_id = models.IntegerField()
+    submit_date = models.DateField()
+    response = models.TextField()
+    score = models.DecimalField(max_digits=5, decimal_places=2)
+    student_name = models.CharField(max_length=50)
