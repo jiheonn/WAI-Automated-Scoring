f3f4e0a660c8fa5ba7b77ea7653fb74f35430ef1
Sungchul Choi <sc82.choi@gachon.ac.kr>
2020-07-16 12:49:17 +0900

    Apply black formatting to initla admin page

--- app/sysop/urls.py
+++ app/sysop/urls.py
@@ -3,17 +3,25 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sysop_login/', views.sysop_login, name='sysop_login'),
-    path('sysop_logout/', views.sysop_logout, name='sysop_logout'),
-    path('', views.home, name='sysop'),
-    path('teacher_data/', views.teacher_data, name='teacher_data'),
-    path('change_approve_0_to_1/', views.change_approve_0_to_1, name='change_approve_0_to_1'),
-    path('change_approve_1_to_0/', views.change_approve_1_to_0, name='change_approve_1_to_0'),
-    path('quiz_review/', views.quiz_review, name='quiz_review'),
-    path('quiz_produce/', views.quiz_produce, name='quiz_produce'),
-    path('notice/', views.notice, name='notice'),
-    path('detailed_review/', views.detailed_review, name='detailed_review'),
-    path('detailed_quiz/', views.detailed_quiz, name='detailed_quiz'),
-    path('write_quiz/', views.write_quiz, name='write_quiz'),
-    path('ques_detail/', views.ques_detail, name='ques_detail')
+    path("sysop_login/", views.sysop_login, name="sysop_login"),
+    path("sysop_logout/", views.sysop_logout, name="sysop_logout"),
+    path("", views.home, name="sysop"),
+    path("teacher_data/", views.teacher_data, name="teacher_data"),
+    path(
+        "change_approve_0_to_1/",
+        views.change_approve_0_to_1,
+        name="change_approve_0_to_1",
+    ),
+    path(
+        "change_approve_1_to_0/",
+        views.change_approve_1_to_0,
+        name="change_approve_1_to_0",
+    ),
+    path("quiz_review/", views.quiz_review, name="quiz_review"),
+    path("quiz_produce/", views.quiz_produce, name="quiz_produce"),
+    path("notice/", views.notice, name="notice"),
+    path("detailed_review/", views.detailed_review, name="detailed_review"),
+    path("detailed_quiz/", views.detailed_quiz, name="detailed_quiz"),
+    path("write_quiz/", views.write_quiz, name="write_quiz"),
+    path("ques_detail/", views.ques_detail, name="ques_detail"),
 ]
--- app/sysop/views.py
+++ app/sysop/views.py
@@ -9,6 +9,7 @@ from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
+
 @method_decorator(csrf_exempt)
 def sysop_login(request):
     if request.method == "POST":
@@ -20,100 +21,104 @@ def sysop_login(request):
                 login(request, user)
                 return render(request, "sysop/home.html")
             else:
-                messages.error(request, '선생님은 관리자페이지에 접속하실 수 없습니다.')
+                messages.error(request, "선생님은 관리자페이지에 접속하실 수 없습니다.")
                 return render(request, "sysop/sysop_login.html")
         else:
-            messages.error(request, '아이디 또는 비밀번호가 일치하지 않습니다.')
+            messages.error(request, "아이디 또는 비밀번호가 일치하지 않습니다.")
             return render(request, "sysop/sysop_login.html")
 
     return render(request, "sysop/sysop_login.html")
+
 
 # 로그아웃 함수
 def sysop_logout(request):
     logout(request)
     return redirect("sysop_login")
 
+
 def home(request):
-    context = {
-    }
-    return render(request, 'sysop/home.html', context)
+    context = {}
+    return render(request, "sysop/home.html", context)
 
 
 def teacher_data(request):
     teacher = Teacher.objects.all()
-    context = {'teacher':teacher}
-    return render(request, 'sysop/teacher_data.html', context)
+    context = {"teacher": teacher}
+    return render(request, "sysop/teacher_data.html", context)
+
 
 def change_approve_0_to_1(request):
-    teacher_id = request.GET.get('teacher_id')
+    teacher_id = request.GET.get("teacher_id")
     teacher_info = Teacher.objects.get(teacher_id=teacher_id)
     teacher_info.approve = 1
     teacher_info.save()
     teacher = Teacher.objects.all()
 
-    context = {'teacher':teacher}
-    return render(request, 'sysop/teacher_data.html', context)
+    context = {"teacher": teacher}
+    return render(request, "sysop/teacher_data.html", context)
+
 
 def change_approve_1_to_0(request):
-    teacher_id = request.GET.get('teacher_id')
+    teacher_id = request.GET.get("teacher_id")
     teacher_info = Teacher.objects.get(teacher_id=teacher_id)
     teacher_info.approve = 0
     teacher_info.save()
     teacher = Teacher.objects.all()
 
-    context = {'teacher':teacher}
-    return render(request, 'sysop/teacher_data.html', context)
+    context = {"teacher": teacher}
+    return render(request, "sysop/teacher_data.html", context)
+
 
 def quiz_review(request):
-    makequestion = MakeQuestion.objects.select_related('teacher')
-    context = {'makequestion':makequestion
-    }
-    return render(request, 'sysop/quiz_review.html', context)
+    makequestion = MakeQuestion.objects.select_related("teacher")
+    context = {"makequestion": makequestion}
+    return render(request, "sysop/quiz_review.html", context)
+
 
 def quiz_produce(request):
     question = Question.objects.select_related()
-    context = {'question':question
-    }
-    return render(request, 'sysop/quiz_produce.html', context)
+    context = {"question": question}
+    return render(request, "sysop/quiz_produce.html", context)
+
 
 def notice(request):
-    context = {
-    }
-    return render(request, 'sysop/notice.html', context)
+    context = {}
+    return render(request, "sysop/notice.html", context)
+
 
 def detailed_review(request):
-    question_id = request.GET['question_id']
-    makequestion = MakeQuestion.objects.select_related('teacher').filter(make_question_id=question_id)[0]
-    context = {'makequestion':makequestion
-    }
-    return render(request, 'sysop/detailed_review.html', context)
+    question_id = request.GET["question_id"]
+    makequestion = MakeQuestion.objects.select_related("teacher").filter(
+        make_question_id=question_id
+    )[0]
+    context = {"makequestion": makequestion}
+    return render(request, "sysop/detailed_review.html", context)
+
 
 def detailed_quiz(request):
-    question_id = request.GET.get('question_id')
+    question_id = request.GET.get("question_id")
     question = []
     for i in question_id:
-        question = Question.objects.select_related('category').filter(question_id=i)[0]
-    context = {'question':question
-    }
-    return render(request, 'sysop/detailed_quiz.html', context)
+        question = Question.objects.select_related("category").filter(question_id=i)[0]
+    context = {"question": question}
+    return render(request, "sysop/detailed_quiz.html", context)
+
 
 def write_quiz(request):
-    context = {
-    }
-    return render(request, 'sysop/write_quiz.html', context)
+    context = {}
+    return render(request, "sysop/write_quiz.html", context)
+
 
 def ques_review(request):
     question = Question.objects.select_related()
-    context = {'question':question
-    }
-    return render(request, 'sysop/ques_review.html', context)
+    context = {"question": question}
+    return render(request, "sysop/ques_review.html", context)
+
 
 def ques_detail(request):
-    question_id = request.GET.get('question_id')
+    question_id = request.GET.get("question_id")
     question = []
     for i in question_id:
-        question = Question.objects.select_related('category').filter(question_id=i)[0]
-    context = {'question':question
-    }
-    return render(request, 'sysop/ques_detail.html', context)
-
+        question = Question.objects.select_related("category").filter(question_id=i)[0]
+    context = {"question": question}
+    return render(request, "sysop/ques_detail.html", context)
