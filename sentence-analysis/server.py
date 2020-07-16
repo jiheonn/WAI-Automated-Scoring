a7cc88b289a0db24656043d3a98ae19518ca4137
Sungchul Choi <sc82.choi@gachon.ac.kr>
2020-07-16 10:53:45 +0900

    Update sentence-analysis api code for formmatting

--- sentence-analysis/server.py
+++ sentence-analysis/server.py
@@ -6,34 +6,38 @@ import argparse
 import logging
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'qvm'
+app.config["SECRET_KEY"] = "qvm"
 socketio = SocketIO(app)
 
 sa_logger = logging.getLogger("sentence-analysis")
 sa_logger.setLevel(logging.INFO)
-logger_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger_format = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(logger_format)
 sa_logger.addHandler(stream_handler)
 
 
-@app.route('/get-tokenized', methods=['POST'])
+@app.route("/get-tokenized", methods=["POST"])
 def get_tokenized():
     sa_logger.info("get-tokenized")
     requested_data = request.get_json()
-    raw_sentence = requested_data['raw_sentence']
-    print(f'[INPUT] raw_sentence : {raw_sentence}')
+    raw_sentence = requested_data["raw_sentence"]
+    print(f"[INPUT] raw_sentence : {raw_sentence}")
     result, most_common_word = bigram.get_tokenized_words(raw_sentence)
     context = {
-        'tokenized': result,
-        'most_common': most_common_word,
+        "tokenized": result,
+        "most_common": most_common_word,
     }
     return context
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Arguments for Sentence-Analysis Server.')
-    parser.add_argument('--port', type=int, default=5252, help='port number for sentence-analysis server.')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Arguments for Sentence-Analysis Server."
+    )
+
     args = parser.parse_args()
     sa_logger.info("Start Sentence-Analysis Server.")
-    socketio.run(app, host="0.0.0.0", port=args.port, debug=True)
+    socketio.run(app, host="0.0.0.0", debug=True)
