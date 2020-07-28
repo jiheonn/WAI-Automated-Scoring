608a965bc458587d6bed9b389c9fc3ff0724d526
Wonyoungpark <holly67827781@gmail.com>
2020-07-28 14:49:42 +0900

    좋은 설명 파일이름 변경

--- app/student/urls.py
+++ app/student/urls.py
@@ -6,18 +6,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # 좋은 설명 페이지
-    path("", views.good_ex, name="student"),
-    # 평가연습 페이지
-    path("evaluate_exercise/", views.evaluate_exercise, name="STU_evaluate_exercise"),
-    path("evaluate_exercise/question/", views.evaluate_exercise_question, name="STU_evaluate_exercise_question"),
-    path("evaluate_exercise/question/diagnosis/", views.evaluate_exercise_diagnosis, name="STU_evaluate_exercise_diagnosis"),
-    # 학습평가 페이지
-    path("Study/", views.study_evaluate, name="Study"), #study_evaluate
+    path("", views.good_explain, name="student"),
+    path("AI/", views.ai, name="AI"),
+    path("AI/AIques/", views.ai_question, name="AIquestion"),
+    path("AI/AIques/AIdiag/", views.ai_diagnosis, name="AIdiagnosis"),
+    path("Study/", views.study_evaluate, name="Study"),
     path("Study/Studyques/", views.study_evaluate_question, name="Studyquestion"),
-    # 숙제하기, 숙제 조회 중 선택 페이지
     path("Homeworkselect/", views.homework_do_check_select, name="Homeworkselect"),
-    # 숙제 조회 페이지
-    path("Homework/", views.homework_check_id, name="Homework"), #check_homework
+    path("Homework/", views.homework_check_id, name="Homework"),
     path(
         "Homeworkselect/Homework/Homeworklist/", views.homework_check_list, name="Homeworklist"
     ),
@@ -26,8 +22,7 @@ urlpatterns = [
         views.homework_check_homework,
         name="Homeworkcheck",
     ),
-    # 숙제하기 페이지
-    path("Homeworkselect/Homeworkcode/", views.homework_do_code, name="Homeworkcode"), #do_homework
+    path("Homeworkselect/Homeworkcode/", views.homework_do_code, name="Homeworkcode"),
     path(
         "Homework/Homeworkselect/Homeworkcode/Homeworkques/",
         views.homework_do_question,
@@ -38,14 +33,11 @@ urlpatterns = [
         views.homework_do_diagnosis,
         name="Homeworkdiagnosis",
     ),
-    # 스스로 평가 페이지
-    path("Self/", views.self, name="Self"), #self_evaluate
+    path("Self/", views.self, name="Self"),
     path("Self/Selfques/", views.self_question, name="Selfquestion"),
     path("Self/Selfques/Selfdiag/", views.self_diagnosis, name="Selfdiagnosis"),
     path("Self/Selfques/Selfdiag/Selfgrade/", views.self_grade_score, name="Selfgrade"),
-    # 게시판 페이지
     path("Notice/", views.notice, name="Notice"),
-    # 함수
     path("search/", views.search_keyword, name="search"),
     path("search_name/", views.search_name, name="search_name"),
     path("change_category/", views.change_category_ai, name="change_category"),
--- app/student/views.py
+++ app/student/views.py
@@ -9,23 +9,21 @@ from django.db.models import Q
 import datetime
 import json
 
-# TODO : import 세분화하
 from mainpage.models import Question, SelfSolveData, AssignmentQuestionRel, Keyword, Solve, Assignment, MakeQuestion, \
     Category, StudySolveData, Mark
 
 INIT_SCORE = 0
 
 
-# TODO : 함수명 구체적으로 바꾸기
 # 좋은 설명
-def good_ex(request):
+def good_explain(request):
     context = {
     }
-    return render(request, 'student/index.html', context)
+    return render(request, 'student/good_explain.html', context)
 
 
 # 평가연습
-def evaluate_exercise(request):
+def ai(request):
     qs = Question.objects.all()
     category = Category.objects.all()
 
@@ -33,21 +31,21 @@ def evaluate_exercise(request):
         'qs': qs,
         'category': category
     }
-    return render(request, 'student/evaluate_exercise.html', context)
+    return render(request, 'student/AI.html', context)
 
 
 # 평가연습 문항 페이지
-def evaluate_exercise_question(request):
+def ai_question(request):
     question_id = int(request.GET['question_id'])
     data = Question.objects.filter(question_id=question_id).first()
     context = {
         'data': data
     }
-    return render(request, 'student/evaluate_exercise_question.html', context)
+    return render(request, 'student/AIques.html', context)
 
 
 # 평가연습 피드백 페이지
-def evaluate_exercise_diagnosis(request):
+def ai_diagnosis(request):
     question_id = request.GET['question_id']
     ques_ans = request.GET['ques_ans']
 
@@ -65,7 +63,6 @@ def evaluate_exercise_diagnosis(request):
         school = ""
         gender = ""
 
-    # TODO : 변수명 바꾸기
     join_aqr_q = AssignmentQuestionRel.objects.select_related('question').filter(question__question_id=question_id)
     data = join_aqr_q.first()
 
@@ -91,7 +88,7 @@ def evaluate_exercise_diagnosis(request):
     except:
         study_solve_data = None
 
-    return render(request, 'student/evaluate_exercise_diagnosis.html', context)
+    return render(request, 'student/AIdiag.html', context)
 
 
 # 학습평가 코드 입력 페이지
