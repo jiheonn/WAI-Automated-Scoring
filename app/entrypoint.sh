47467e99845a6a82c62523844c2539ce1103db36
JONGSKY <ljhljh0125@gmail.com>
2020-09-09 22:27:41 +0900

    불필요한 admin 주석 처리 및 계정이름 재설정

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -23,9 +23,9 @@ python manage.py migrate
 python manage.py loaddata init_mainpage.json
 python manage.py loaddata init_teacher.json
 python manage.py collectstatic --no-input --clear
-echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_superuser('admin1', 'admin1@gmail.com', 'teamlab')" | python manage.py shell
-echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_superuser('admin2', 'admin2@gmail.com', 'teamlab')" | python manage.py shell
-echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_superuser('admin3', 'admin3@gmail.com', 'teamlab')" | python manage.py shell
-echo "from teacher.models import User; user = User.objects.create_user(username='hong1@naver.com', password='1431'); user.last_name='홍'; user.first_name='길동1'; user.teacher_id='1'; user.save()" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_superuser('test-admin', 'admin@gmail.com', 'test1234')" | python manage.py shell
+# echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_superuser('admin2', 'admin2@gmail.com', 'teamlab')" | python manage.py shell
+# echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_superuser('admin3', 'admin3@gmail.com', 'teamlab')" | python manage.py shell
+echo "from teacher.models import User; user = User.objects.create_user(username='test-teacher@gmail.com', password='test1234'); user.last_name='test-'; user.first_name='teacher'; user.teacher_id='1'; user.save()" | python manage.py shell
 
 exec "$@"
