7a70ef9f0572333135abd345e0712dcee119c9c6
JONGSKY <ljhljh0125@gmail.com>
2020-09-10 17:03:06 +0900

    주석코드 삭제 및 한줄비우기

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -24,8 +24,7 @@ python manage.py loaddata init_mainpage.json
 python manage.py loaddata init_teacher.json
 python manage.py collectstatic --no-input --clear
 echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_superuser('test-admin', 'admin@gmail.com', 'test1234')" | python manage.py shell
-# echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_superuser('admin2', 'admin2@gmail.com', 'teamlab')" | python manage.py shell
-# echo "from django.contrib.auth import get_user_model; CustomUser = get_user_model();  CustomUser.objects.create_superuser('admin3', 'admin3@gmail.com', 'teamlab')" | python manage.py shell
+
 echo "from teacher.models import User; user = User.objects.create_user(username='test-teacher@gmail.com', password='test1234'); user.last_name='test-'; user.first_name='teacher'; user.teacher_id='1'; user.save()" | python manage.py shell
 
 exec "$@"
